256611b34a51d2b78114a827115fa48fb9cc7f9e
asatake <asataken@gmail.com>
2016-04-23 15:31:13 +0900

    .nojekyll抜くテスト

--- js/sazae.js
+++ js/sazae.js
@@ -1,10 +1,19 @@
+function person(id, name, parent, marrige, children){
+    this.id = id;
+    this.name = name;
+    this.parent = parent;
+    this.marrige = marrige;
+    this.children = children;
+}
+
 httpObj = new XMLHttpRequest();
 httpObj.open("get", "data/family.json");
 httpObj.onload = function(){
     var data = JSON.parse(this.responseText);
     var txt = "";
     for(var i = 0; i < data.list.length; i++){
-        txt = txt + data.list[i].id + " " + data.list[i].name + "<br />";
+        txt = txt + data.list[i].id + " " + data.list[i].name + " " + data.list[i].parent + "<br />";
+        
     }
     document.getElementById("result").innerHTML = txt;
 };
