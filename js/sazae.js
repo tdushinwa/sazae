14f7324763279cf98f85402625e1e969c02a4f79
FujiLab <fujimotoLab>
2016-04-27 17:46:02 +0900

    情報を分けた

--- js/sazae.js
+++ js/sazae.js
@@ -1,20 +1,37 @@
+// 描画に最低限の情報を読み込む
 var node = [];
-
 httpObj = new XMLHttpRequest();
+httpObj.open("get", "data/tree.json");
+httpObj.onload = function(){
+    var data = JSON.parse(this.responseText);
+    var txt = "";
+    for(var i = 0; i < data.list.length; i++){
+        // 扱いやすいように整形
+        node.push({"id": data.list[i].id,
+                   "parent": data.list[i].parent,
+                    "marrige": data.list[i].marrige,
+                    "children":data.list[i].children}
+                  );
+    }
+};
+httpObj.send(null);
+httpObj.close();
+
+// 各ノードの詳細を格納する
+var detail = [];
 httpObj.open("get", "data/family.json");
 httpObj.onload = function(){
     var data = JSON.parse(this.responseText);
     var txt = "";
     for(var i = 0; i < data.list.length; i++){
-        txt = txt + data.list[i].id + " " + data.list[i].name + " " + data.list[i].parent + "<br />";
         // 扱いやすいように整形
-        node.push({"id": data.list[i].id,
-                    "name": data.list[i].name,
-                    "parent": data.list[i].parent,
-                    "marrige": data.list[i].marrige,
-                    "children":data.list[i].children}
-                  );
+        detail.push({"id": data.list[i].id,
+                   "name":data.list[i].name}
+                 );
     }
-    document.getElementById("result").innerHTML = txt;
 };
 httpObj.send(null);
+httpObj.close();
+
+// 階層構造を保持する
+var path = [];
