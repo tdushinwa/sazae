2c6694e23869312d7796b5a1f42f63b0edcea2b6
asatake <asataken@gmail.com>
2016-05-04 21:48:12 +0900

    ちょっとしたテスト

--- js/sazae.js
+++ js/sazae.js
@@ -1,40 +1,7 @@
-function treeNode(id, parent, marrige, children, x, y, z){
-    this.id = id;
-    this.parent = parent;
-    this.marrige = marrige;
-    this.children = children;
-    this.x = x;
-    this.y = y;
-    this.z = z;
-}
-
 // 描画に最低限の情報を読み込む
 var url = "data/tree.json";
-// var httpObj = new XMLHttpRequest();
-
-// httpObj.onload = function(){
-//     if(this.readyState == 4 && this.status == 200){
-//         console.log(httpObj.getResponseHeader('Content-Type'));
-//         if(this.getResponseHeader('Content-Type').indexOf('application/json') != -1){
-//             var obj;
-//             if(window.JSON){
-//                 obj = JSON.parse(httpObj.responseText);
-//                 document.write("foo");
-//             }
-//             else{
-//                 obj = eval('(' + httpObj.responseText + ')');
-//                 document.write("bar");
-//             }
-//         }
-//     }
-// };
-
-// httpObj.open("GET", url, true);
-// if(httpObj.overrideMimeType){
-//     httpObj.overrideMimeType("application/json");
-// }
-// httpObj.responseType = 'json';
-// httpObj.send(null);
-$.getJSON(url, function(){
-    alert("ok");
+var obj;
+$.getJSON(url, function(data){
+    console.log(data.list[0].id);
+    obj = data;
 });
