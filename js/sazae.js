406fc82d2ad4156ea95958aab10e5328b42976f3
asatake <asataken@gmail.com>
2016-05-05 01:14:03 +0900

    試行錯誤

--- js/sazae.js
+++ js/sazae.js
@@ -1,7 +1,11 @@
+// 描画関連の初期化
+var scene = new THREE.Scene();
+var camera = new THREE.Camera();
+var renderer = new THREE.WebGLRenderer();
+
 // 描画に最低限の情報を読み込む
 var url = "data/tree.json";
-var obj;
 $.getJSON(url, function(data){
     console.log(data.list[0].id);
-    obj = data;
 });
+
