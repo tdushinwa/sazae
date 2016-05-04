76951df269bacc7e5a0a50f7dd9d96e20f46f1b9
asatake <asataken@gmail.com>
2016-05-04 21:18:43 +0900

    ちょっとしたテスト

--- js/sazae.js
+++ js/sazae.js
@@ -1,37 +1,40 @@
+function treeNode(id, parent, marrige, children, x, y, z){
+    this.id = id;
+    this.parent = parent;
+    this.marrige = marrige;
+    this.children = children;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
 // 描画に最低限の情報を読み込む
-var node = [];
-httpObj = new XMLHttpRequest();
-httpObj.open("get", "data/tree.json");
-httpObj.onload = function(){
-    var data = JSON.parse(this.responseText);
-    var txt = "";
-    for(var i = 0; i < data.list.length; i++){
-        // 扱いやすいように整形
-        node.push({"id": data.list[i].id,
-                   "parent": data.list[i].parent,
-                    "marrige": data.list[i].marrige,
-                    "children":data.list[i].children}
-                  );
-    }
-};
-httpObj.send(null);
-httpObj.close();
+var url = "data/tree.json";
+// var httpObj = new XMLHttpRequest();
 
-// 各ノードの詳細を格納する
-var detail = [];
-httpObj.open("get", "data/family.json");
-httpObj.onload = function(){
-    var data = JSON.parse(this.responseText);
-    var txt = "";
-    for(var i = 0; i < data.list.length; i++){
-        // 扱いやすいように整形
-        detail.push({"id": data.list[i].id,
-                   "name":data.list[i].name}
-                 );
-    }
-};
-httpObj.send(null);
-httpObj.close();
+// httpObj.onload = function(){
+//     if(this.readyState == 4 && this.status == 200){
+//         console.log(httpObj.getResponseHeader('Content-Type'));
+//         if(this.getResponseHeader('Content-Type').indexOf('application/json') != -1){
+//             var obj;
+//             if(window.JSON){
+//                 obj = JSON.parse(httpObj.responseText);
+//                 document.write("foo");
+//             }
+//             else{
+//                 obj = eval('(' + httpObj.responseText + ')');
+//                 document.write("bar");
+//             }
+//         }
+//     }
+// };
 
-// 階層構造を保持する
-var path = [];
+// httpObj.open("GET", url, true);
+// if(httpObj.overrideMimeType){
+//     httpObj.overrideMimeType("application/json");
+// }
+// httpObj.responseType = 'json';
+// httpObj.send(null);
+$.get(url, function(){
+    alert("ok");
+});
